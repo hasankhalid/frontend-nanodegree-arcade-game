50c8ddf4e97e559fa7ac7b3d4b354151286cd331
hasankhalid <hasankhalid940@gmail.com>
2017-01-17 23:20:09 +0500

    master

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 frontend-nanodegree-arcade-game
 ===============================
 
+##Description
+The came developed in this project is a slight variant of the classic arcade game called Frogger.
+
+##How to Play?
+Just use the arrow keys for navigation and move across the map to the very top. For every collision with the enemy bug your position will be reset to your starting position and 5 points will be deducted from your total score but do not be disheartened, every time you reach the top 20 points will be added to your tally. You can move across all directions.
+
+##Navigation and Movement
+Up Arrow - Move upwards.
+Down Arrow - Move Downwards.
+Left Arrow - Move left.
+Right Arrow - Move right.
+
+##Game Installation
+Just download the zip file and extract it. Run index.html via your browser and the game should function properly.
+
+## Tools / Techniques
+- Object-Oriented JavaScript
+- constructor functions to create multiple Enemy instances
+- prototypal inheritance
+- HTML5 Canvas API
+- resource loading via immediately-invoked function expressions (IIFE)
+
+##Project Requirements
+[https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797/](https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797/ "Frogger Arcade Game Project Requirements")
+
+##Additional Resources used.
+http://stackoverflow.com/questions/14094697/javascript-how-to-create-new-div-dynamically-change-it-move-it-modify-it-in
+https://davidwalsh.name/fix-javascript-errors
+http://thecodeplayer.com/walkthrough/html5-game-tutorial-make-a-snake-game-using-html5-canvas-jquery
+https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript
+
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
+var numberOfEnemies;
+var allEnemies = [];
+var player;
+var enemy;
+var possibleY = [40, 135, 210];
+
+//Variables declared!
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(xPosition, yPosition, velocity) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.xPos = xPosition;
+    this.yPos = yPosition;
+    this.vel = velocity;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,22 +24,115 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    var difference = this.vel * dt;
+    this.xPos += difference;
+    //Use the dt parameter for movement.
+    if (this.xPos >= 505){
+        this.xPos = -50;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.xPos, this.yPos);
+    this.checkCollision();
+};
+
+//Function checkCollision takes an enemy bug as an input and checks collisions with the player object!
+Enemy.prototype.checkCollision = function(){
+  if (player.yPos <= this.yPos + 50 && player.xPos <= this.xPos + 75 && player.xPos+60 >= this.xPos && player.yPos + 50 >= this.yPos) {
+      //decrement the score by 5 in case of collision
+      player.score -= 5;
+      console.log("5 Points reduced on collision");
+      console.log("Score: " + player.score);
+      console.log("Level: " + player.level);
+      player.reset();
+      //Reset the position of the player in case of collision
+    }
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(xPosition, yPosition, velocity) {
+    //The player class declares the characteristics of the Player
+    this.xPos = xPosition;
+    this.yPos = yPosition;
+    this.vel = velocity;
+    this.sprite = 'images/char-boy.png';
+    this.score;
+    this.level;
+};
+
+Player.prototype.update = function(){
+    //Following if statement increments score once the doodle reaches the top of the canvas.
+    if (this.yPos <= 0){
+        this.reset();
+        this.score += 20;
+        numberOfEnemies += 1;
+        this.level += 1;
+        addEnemy();
+        console.log("Score:" + this.score);
+        console.log("Level: " + this.level);
+    }
+    //Following three if cases prevent the doodle from moving off the screen
+    if (this.xPos >= 402.5){
+        this.xPos = 402.5;
+    }
+    if (this.xPos <= 2.5){
+        this.xPos = 2.5;
+    }
+    if (this.yPos >= 435){
+        this.yPos = 435;
+    }
+};
+
+//Following function will reset the player to default location
+Player.prototype.reset = function(){
+    this.xPos = 202.5;
+    this.yPos = 410;
+};
+
+//This method increases the number of enemies which is passed as a parameter. It gives them random values for Y axis and velocity.
+function addEnemy(){
+      allEnemies.push(new Enemy(-50, possibleY[Math.floor(Math.random() *3) + 0], Math.floor(Math.random() * 175) + 25));
+};
+
+//This is a required method
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.xPos, this.yPos);
+};
+
+Player.prototype.handleInput = function(key){
+    if (key == 'up'){
+        this.yPos -= 25;
+    }
+    if (key == 'down'){
+        this.yPos += 25;
+    }
+    if (key == 'left'){
+        this.xPos -= 25;
+    }
+    if (key == 'right'){
+        this.xPos += 25; //Change the players velocity parameter upon keyPress
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+//Here the initialize method instantiates the variables and objects.
+function initialize() {
+    player = new Player(202.5,410,50);
+    enemy = new Enemy(-50, possibleY[Math.floor(Math.random() *3) + 0], Math.floor(Math.random() * 200) + 25);
+    player.score = 0;
+    player.level = 1;
+    allEnemies.push(enemy);
+};
+
+initialize();
 
 
 // This listens for key presses and sends the keys to your
